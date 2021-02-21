3b55880bb6a1a1e9246caa5ed53f1915a0ad0801
Mikhael <mikhaelapg00@gmail.com>
2021-02-21 23:16:27 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('title', 'Index')
+    
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+            </div>
+        </div>
+    </div>
+@endsection
