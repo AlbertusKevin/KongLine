c01d8c9ff35acf5cf00a3e39475b51b306330749
Mikhael <mikhaelapg00@gmail.com>
2021-02-23 16:32:22 +0700

    detail petisi update

--- resources/views/petitionDetail.blade.php
+++ resources/views/petitionDetail.blade.php
@@ -9,11 +9,27 @@
             <button type="button" class="btn btn-light rounded-pill ml-3">Ikut Serta</button>
         </div>
         <div class="row">
-            <div class="col-8">
+            <div class="col-md-8 mt-3">
                 <hr>
                 <img src="/img/detailPetisi.png" alt="detail petisi">
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus sequi cupiditate veniam esse rerum. Consequuntur, velit vitae! Natus sed odio maiores? Consectetur ut architecto aperiam quisquam vero recusandae veritatis maiores provident a, reiciendis fuga culpa cupiditate sapiente voluptatibus perspiciatis sunt ad quae deserunt voluptas ipsa molestiae iure velit. Odio.</p>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugiat dolorum voluptatibus labore quod dolorem non consequatur tempore veritatis aperiam totam animi ab pariatur corrupti iusto magnam delectus accusamus aspernatur corporis! Possimus delectus libero adipisci dolore at porro commodi illo.</p>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus sequi cupiditate veniam esse rerum. Consequuntur, velit vitae! Natus sed odio maiores? Consectetur ut architecto aperiam quisquam vero recusandae veritatis maiores provident a, reiciendis fuga culpa cupiditate sapiente voluptatibus perspiciatis sunt ad quae deserunt voluptas ipsa molestiae iure velit. Odio.</p>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugiat dolorum voluptatibus labore quod dolorem non consequatur tempore veritatis aperiam totam animi ab pariatur corrupti iusto magnam delectus accusamus aspernatur corporis! Possimus delectus libero adipisci dolore at porro commodi illo.</p>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus sequi cupiditate veniam esse rerum. Consequuntur, velit vitae! Natus sed odio maiores? Consectetur ut architecto aperiam quisquam vero recusandae veritatis maiores provident a, reiciendis fuga culpa cupiditate sapiente voluptatibus perspiciatis sunt ad quae deserunt voluptas ipsa molestiae iure velit. Odio.</p>
+                <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, fugiat dolorum voluptatibus labore quod dolorem non consequatur tempore veritatis aperiam totam animi ab pariatur corrupti iusto magnam delectus accusamus aspernatur corporis! Possimus delectus libero adipisci dolore at porro commodi illo.</p>
             </div>
-            <div class="col-4">col-4</div>
+            <div class="col-md-4">
+                <h4 class="mt-5 ml-4">22 Ribu  dari 50 Ribu </h4>
+                <p class="ml-4">Orang telah menendatangani Petisi ini !</p>
+                <div class="row row-cols-2">
+                    <div class="col-sm-4"><img src="/img/detailProfile.png" alt="petition profile" class="ml-4"></div>
+                    <div class="col-sm-8"><b>Ruben Calzoni</b><input class="form-control form-control-sm form-rounded mt-2" type="text" placeholder="Tulis Komentarmu"></div>
+                </div>
+                <div class="row">
+                    
+                </div>
+            </div>
           </div>
     </div>
 @endsection
