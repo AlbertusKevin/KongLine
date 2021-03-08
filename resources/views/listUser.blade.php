457f9b0d448b3665608b746f1a11c22ffe7143af
Hizkiaandrew <hizkiaandrew@gmail.com>
2021-03-09 00:55:24 +0700

    UI ketinggalan

--- resources/views/listUser.blade.php
+++ resources/views/listUser.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
 <div class="container">
+    <div class="text-center mt-5">
+        <button href="/petisi" type="button" class="btn btn-primary petition-type rounded-pill">Berlangsung</button>
+        <button href="/petisi" type="button" class="btn btn-light petition-type rounded-pill ml-3">Telah Menang</button>
+        @if (Auth::check())
+            @if (Auth::user()->role == 'participant' || Auth::user()->role == 'campaigner')
+                <button href="/petisi" type="button" class="btn btn-light petition-type rounded-pill ml-3">Ikut
+                    Serta</button>
+            @endif
+            @if (Auth::user()->role == 'campaigner')
+                <button type="button" class="btn btn-light petition-type rounded-pill ml-3">Petisi Saya</button>
+            @endif
+        @endif
+    </div>
+    <div class="form-inline my-2 my-lg-0 justify-content-center">
+        <input class="form-control mr-sm-2 w-50 mt-5" id="search-petition" type="search" placeholder="Search"
+            aria-label="Search">
+        <input type="hidden" id="sort-by" value="None">
+        <input type="hidden" id="category-choosen" value="None">
+        <div class="dropdown mt-5 mr-2">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="sort-by" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Sort By
+            </button>
+            <div class="dropdown-menu" aria-labelledby="sort-by">
+                <a class="dropdown-item sort-petition font-weight-bold">None</a>
+                <a class="dropdown-item sort-petition">Jumlah Tanda Tangan</a>
+                <a class="dropdown-item sort-petition">Event Terbaru</a>
+            </div>
+        </div>
+        
     <table class="table table-borderless">
         <thead style="background-color:#E2E2E2">
           <tr class="text-center font-weight-normal">
